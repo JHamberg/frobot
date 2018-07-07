7666f936a2e3b8a99793bedcee51a85d2dd242c7
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2018-07-07 22:52:45 +0300

    Add initial kaomoji commands: list and randomoji

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const Discord = require('discord.js');
-require('dotenv').config()
+const fs = require("fs");
+require('dotenv').config();
 
 const client = new Discord.Client();
 const prefix = process.env.PREFIX;
+let kaomojis = [];
 
 client.on("ready", () => {
     console.log(`Bot started with ${client.users.size} users!`);
     client.user.setActivity(process.env.STATUS);
+
+    // Load available kaomojis
+    fs.readFile(__dirname + "/kaomojis.txt", "utf8", (err, contents) => {
+        if(err) throw err;
+        kaomojis = contents.split("\n");
+        console.log(kaomojis);
+    });
 });
 
 client.on("message", async msg => {
@@ -14,11 +23,19 @@ client.on("message", async msg => {
     if(msg.author.bot) return;
     if(!msg.content.includes(prefix)) return;
     
+    // Split the command from its arguments
     const args = msg.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
+
+    // Process the command
     switch(cmd){
+        case "list":
+            const list = kaomojis.join("\n");
+            msg.channel.send(`Available kaomojis:\`\`\`\n${list}\n\`\`\``);
+            break;
         case "randomoji":
-            msg.channel.send("Under construction");
+            const idx = Math.floor(Math.random() * kaomojis.length);
+            msg.channel.send(kaomojis[idx]);
             break;
         case "latency":
         case "delay":

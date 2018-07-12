a2f07afe6bb8b4290037e293622d734ff6ae2139
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2018-07-12 22:09:12 +0300

    Import promisify in the correct module

--- io/fsp.js
+++ io/fsp.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
+const {promisify} = require('util');
 
 // Basic wrapper to promisify selected fs functions 
 const fsp = {
     readdir: promisify(fs.readdir),
     mkdir: promisify(fs.mkdir),
     exists: promisify(fs.exists),
+    writeFile: promisify(fs.writeFile),
     readFile: promisify(fs.readFile),
     open: promisify(fs.open),
     close: promisify(fs.close)

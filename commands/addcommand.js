59d5555f63f2871636635ff37afca52751310899
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2018-07-12 22:51:33 +0300

    Create a command 'addcommand' for adding custom guild-specific commands

---
+++ commands/addcommand.js
@@ -0,0 +1,32 @@
+const guilds = require("../guilds.js");
+
+const addcommand = {
+    aliases: ["addcommand"],
+    description: "Add a custom server-wide command. Usage: !addcommand <command> <output>",
+    run: async (msg, args, client) => {
+        const guild = msg.channel.guild;
+
+        // Validate and parse args
+        if(!args || args.length < 2) return;
+        const name = args[0];
+        const output = args.slice(1).join(" ");
+
+        // Remove unnecessary command prefix
+        if(name.startsWith(process.env.PREFIX)){
+            name = name.slice(1);
+        }
+
+        // Ensure the command does not already exist
+        const commands = guilds.getCommands(guild);
+        if(commands[name]) {
+            await msg.channel.send(`Command ${process.env.PREFIX}${name} already exists!`);
+            return;
+        } 
+
+        // Add the command
+        await guilds.addCommand(guild, name, output);
+        await msg.channel.send("Command added!");
+    }
+};
+
+module.exports = addcommand;

69282f6c0c52f6a55a8a790a5b969def20f168c8
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2018-07-16 18:30:34 +0300

    Create a command 'gif' for fetching random images from Giphy

---
+++ commands/giphy.js
@@ -0,0 +1,23 @@
+const request = require("request-promise-native");
+
+const giphy = {
+    aliases: ["giphy", "gif", "webm"],
+    description: "Look up a random GIF from Giphy",
+    enabled: !!process.env.GIPHY_TOKEN, // Need an API key for this command
+    run: async (msg, args, client) => {
+        const search = args ? encodeURI(args.join(" ")) : "";
+        const options = {
+            url: `http://api.giphy.com/v1/gifs/random?tag=${search}`,
+            json: true,
+            qs: {
+                api_key: process.env.GIPHY_TOKEN,
+                rating: "g" // Content rating
+            }
+        }
+        const response = await request(options);
+        const url = response.data.image_url;  
+        await msg.channel.send(url ? url : "No results found!");
+    }
+}
+
+module.exports = giphy;

35f5c049ed6592e267e30db587bed7e5a8858e64
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2018-07-20 22:36:46 +0300

    Add more useful utility methods like 'countByIdentity'

--- utils.js
+++ utils.js
@@ -1,10 +1,12 @@
 const cardinals = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
 
 const utils = {
+    emojify: (str) => `:${str}:`,
+    round: (num, n) => +(Math.round(`${num}e${n}`) + `e-${n}`),
+    bold: (str) => `**${str}**`,
     capitalize: (str) => {
         return str[0].toUpperCase() + str.slice(1);
     },
-    emojify: (str) => `:${str}:`,
     compassify: (deg) => {
         const idx = Math.floor((deg / 22.5) + 0.5);
         return cardinals[idx % 16];
@@ -17,7 +19,13 @@ const utils = {
             hour12: "true"
         }
         return date.toLocaleString("en-US", options)
-    }
+    },
+    countByIdentity: (arr) => {
+        return arr.reduce((res, elem) => {
+            res[elem] ? res[elem]++ : res[elem] = 1;
+            return res;
+        }, {});
+    },
 }
 
 module.exports = utils;

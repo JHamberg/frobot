be7983887bda9cef2a7ca53b399875b1b8424f6f
Jonatan Hamberg <jonatan.hamberg@outlook.com>
2018-07-22 01:25:13 +0300

    Use the random module and utils in dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -1,43 +1,21 @@
-const crypto = require("crypto");
+const random = require("../random.js");
 const utils = require("../utils.js");
 
 const dice = {
     aliases: ["dice", "throw", "roll", "rolls"],
-    description: "Throw a truly random dice",
+    description: "Roll a truly random dice",
     run: async (msg, args, client) => {
-        // Generate a uniform distribution of truly random rolls
+        const author = msg.author.username;
         const output = await msg.channel.send(":hourglass: Rolling the dice…");
 
+        // Generate a uniform distribution of truly random rolls
         const amount = +args[0] || 1;
-        const rolls = Array(amount).fill().map(() => {
-            const buffer = new Uint8Array(1);
+        const rolls = utils.fill(amount, () => random.int256(1, 6));
 
-            // Since six is not a power of two, we need rejection sampling    
-            let candidate;
-            do {
-                const bytes = crypto.randomBytes(buffer.length);
-                buffer.set(bytes);
-                candidate = bytes[0] % 8 + 1; // Power of two
-            } while (candidate > 6);
-
-            // Use the first random number in bounds
-            return candidate; 
-        });
-
-        // Summarize more than 100 rolls
-        const author = msg.author.username;
-        const results = rolls.length > 100 ? dist(rolls) : rolls.map(utils.bold);
+        // Print distribution for more than 100 rolls
+        const results = rolls.length > 100 ? utils.dist(rolls) : rolls.map(utils.bold);
         await output.edit(`:game_die: **${author}** rolls ${results.join(", ")}.`);
     }
-}
-
-// Format the distribution of dice rolls
-const dist = (rolls) => {
-    const counts = utils.countByIdentity(rolls);
-    return Object.entries(counts).map(([face, count]) => {
-        const percentage = utils.round(count / rolls.length * 100, 2);
-        return `**${face}**: ${count} (${percentage}%)`;
-    });
 }
 
 module.exports = dice;
